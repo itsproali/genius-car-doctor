3c5a880bd08281dc95893ce3597bf517ab08aa78
Mohammad Ali <itsproali@gmail.com>
2022-04-23 18:18:16 +0600

    Load Product with Server from MongoDB

--- genius-server/server.js
+++ genius-server/server.js
@@ -1,14 +1,49 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://itsproali:${process.env.DB_PASS}@cluster1.jggf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("GeniusCarDoctor")
+      .collection("services");
+
+    //   Load All Services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //   Load a service
+    app.get("/service/:id", async (req, res) => {
+      const query = { _id: ObjectId(req.params.id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Genius Server Created");
+  res.send("Genius Server Running");
 });
 
 app.listen(port, () => {
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,8 +3,9 @@ import { useNavigate } from "react-router-dom";
 import "./Service.css";
 
 const Service = ({ service }) => {
-  const { id, name, img, description, price } = service;
+  const {_id, name, img, description, price } = service;
   const navigate = useNavigate();
+
   return (
     <div className="service">
       <img className="w-100" src={img} alt="" />
@@ -13,7 +14,7 @@ const Service = ({ service }) => {
       <p>
         <small>{description}</small>
       </p>
-      <button className="btn btn-primary" onClick={() => navigate(`/service/${id}`)}>
+      <button className="btn btn-primary" onClick={() => navigate(`/service/${_id}`)}>
         Book: {name}
       </button>
     </div>
